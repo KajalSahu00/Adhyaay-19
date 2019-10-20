8ca68dd6e1cf8f6bc4b6d67e6e342f3be9ad87b6
Loagn-47 <ashishofficial47@gmail.com>
2019-10-20 17:39:09 +0530

    loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 
-
+import Loader from './loader';
 import Home from './home';
 import Events from './events';
 
+class  App extends React.Component{
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Route path='/' exact component={Home} />
-        <Route path='/events' exact component={Events} />
-      </BrowserRouter>
-    </>
-  );
+  state = { loading: true };
+
+  sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
+
+  wait = async (milliseconds = 3000) => {
+    await this.sleep(milliseconds);
+    this.setState({
+      loading: false
+    });
+  };
+ 
+  // 4 sec for loading screen
+  componentDidMount(){
+    this.wait(3000);
+  }
+
+  render(){
+      if (this.state.loading) return <Loader/>;
+      return (
+        <>
+          <BrowserRouter>
+            <Route path='/' exact component={Home} />
+            <Route path='/events' exact component={Events} />
+          </BrowserRouter>
+        </>
+     );
+    }
 }
 
 export default App;
